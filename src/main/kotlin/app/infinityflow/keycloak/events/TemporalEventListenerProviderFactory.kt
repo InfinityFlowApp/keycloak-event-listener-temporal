6eab64ddcbfcfb5fa9edf3520126fc6a556e1acb
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2024-04-15 23:37:23 +0000

    Add support for MTLS auth Fixes #25

--- src/main/kotlin/app/infinityflow/keycloak/events/TemporalEventListenerProviderFactory.kt
+++ src/main/kotlin/app/infinityflow/keycloak/events/TemporalEventListenerProviderFactory.kt
@@ -2,8 +2,10 @@ package app.infinityflow.keycloak.events
 
 import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
+import io.temporal.serviceclient.SimpleSslContextBuilder
 import io.temporal.serviceclient.WorkflowServiceStubs
 import io.temporal.serviceclient.WorkflowServiceStubsOptions
+import java.io.FileInputStream
 import org.jboss.logging.Logger
 import org.keycloak.Config
 import org.keycloak.events.EventListenerProvider
@@ -14,14 +16,15 @@ import org.keycloak.models.KeycloakSessionFactory
 class TemporalEventListenerProviderFactory : EventListenerProviderFactory {
     private val _logger = Logger.getLogger(TemporalEventListenerProviderFactory::class.java)
     private var _workflowClient: WorkflowClient? = null
-    private var _server = ""
-    private var _namespace = ""
-    private var _taskQueue = ""
-
+    private var _server = "localhost:7233"
+    private var _namespace = "default"
+    private var _taskQueue = "keycloak"
+    private var _clientCert = ""
+    private var _clientCertKey = ""
 
     override fun create(session: KeycloakSession?): EventListenerProvider {
         if (_logger.isDebugEnabled) {
-            _logger.debugf("Creating %s", TemporalEventListenerProvider::class)
+        _logger.debugf("Creating %s", TemporalEventListenerProvider::class)
         }
         return TemporalEventListenerProvider(_workflowClient!!, _taskQueue)
     }
@@ -31,37 +34,43 @@ class TemporalEventListenerProviderFactory : EventListenerProviderFactory {
             _logger.debugf("Initializing %s", TemporalEventListenerProviderFactory::class)
         }
 
-        _server = config?.get("server") ?: "localhost:7233"
-        _namespace = config?.get("namespace") ?: "default"
-        _taskQueue = config?.get("task-queue") ?: "keycloak"
+        _server = config?.get("server") ?: server
+        _namespace = config?.get("namespace") ?: namespace
+        _taskQueue = config?.get("task-queue") ?: taskQueue
+        _clientCert = config?.get("client-cert") ?: clientCert
+        _clientCertKey = config?.get("client-cert-key") ?: clientCertKey
 
-        val workflowServiceStubsOptions = WorkflowServiceStubsOptions
-            .newBuilder()
-            .setTarget(_server)
-            .build()
-        val workflowServiceStubs = WorkflowServiceStubs
-            .newServiceStubs(workflowServiceStubsOptions)
-        _workflowClient = WorkflowClient
-            .newInstance(workflowServiceStubs, WorkflowClientOptions
-                .newBuilder()
-                .setNamespace(_namespace)
-                .build())
+        val workflowServiceStubsOptionsBuilder = WorkflowServiceStubsOptions.newBuilder()
+            .setTarget(server)
+
+        if (clientCert.isNotEmpty() && clientCertKey.isNotEmpty()) {
+            FileInputStream(clientCert).use { certInputStream ->
+                FileInputStream(clientCertKey).use { keyInputStream ->
+                    val sslContext = SimpleSslContextBuilder.forPKCS8(certInputStream, keyInputStream).build()
+                    workflowServiceStubsOptionsBuilder.setSslContext(sslContext)
+                }
+            }
+        }
+
+        val workflowServiceStubs = WorkflowServiceStubs.newServiceStubs(workflowServiceStubsOptionsBuilder.build())
+        
+        workflowClient = WorkflowClient.newInstance(workflowServiceStubs, WorkflowClientOptions.newBuilder()
+            .setNamespace(namespace)
+            .build())
     }
 
     override fun postInit(factory: KeycloakSessionFactory?) {
-        if (_logger.isDebugEnabled) {
-            _logger.debugf("Global Initialization %s", TemporalEventListenerProviderFactory::class)
-        }
+        _logger.debugf("Global Initialization %s", TemporalEventListenerProviderFactory::class.java)
     }
 
     override fun close() {
         if (_logger.isDebugEnabled) {
             _logger.debugf("Closing %s", TemporalEventListenerProviderFactory::class)
         }
+
+        workflowClient?.close()  // Close the workflow client if it's not null
         _workflowClient = null
     }
 
-    override fun getId(): String {
-        return "temporal"
-    }
+    override fun getId(): String = "temporal"
 }
